b4b0ec36ecc0b7c4eb458b34a19f060b5f0c736a
ssumik <joao.sumi@gmail.com>
2026-06-01 19:39:08 -0300

    feat: add user crud base implementation

--- src/main/java/dev/qdule/infra/persistence/repository/UserRepositoryImpl.java
+++ src/main/java/dev/qdule/infra/persistence/repository/UserRepositoryImpl.java
@@ -2,14 +2,37 @@ package dev.qdule.infra.persistence.repository;
 
 import java.util.Optional;
 
+import dev.qdule.application.exception.UserNotFoundException;
 import dev.qdule.domain.model.User;
 import dev.qdule.domain.repository.UserRepository;
+import dev.qdule.infra.mapper.UserEntityMapper;
+import dev.qdule.infra.persistence.panache.UserRespositoryPanache;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class UserRepositoryImpl implements UserRepository {
+    private UserRespositoryPanache userRespositoryPanache;
+
+    @Inject
+    public UserRepositoryImpl(UserRespositoryPanache userRespositoryPanache) {
+        this.userRespositoryPanache = userRespositoryPanache;
+    }
+
     @Override
     public Optional<User> findById(Long id) {
-        return null;
+        return Optional.of(UserEntityMapper.toDomain(userRespositoryPanache.findById(id)));
+    }
+
+    @Override
+    public User save(User user) {
+        userRespositoryPanache.persist(UserEntityMapper.toEntity(user));
+        return user;
+    }
+
+    @Override
+    public void removeById(Long id) {
+        if (!userRespositoryPanache.deleteById(id))
+            throw new UserNotFoundException(id);
     }
 }
